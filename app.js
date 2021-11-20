529b119820e4a7555dcc10c74c3ab01617274b69
Afiq Nazrie <afnazrie@gmail.com>
2021-11-20 20:34:32 +0800

    feat: add middleware, db connection

--- app.js
+++ app.js
@@ -1,16 +1,27 @@
 // import express from 'express';
 const express = require('express')
+const mongoose = require('mongoose')
 
 const app = express();
 
+// middlewares
+app.use('/posts', () => {
+  console.log('/posts middleware running')
+});
+
 // routes
 app.get('/', (req, res) => {
-    res.send('We are on home')
+  res.send('We are on home')
 });
 
 app.get('/posts', (req, res) => {
-    res.send('We are on posts')
+  res.send('We are on posts')
 });
+
+// connect to db
+mongoose.connect('mongodb://mangodb/?directConnection=true',
+                 () => console.log('Connected to db')
+);
 
 // listen
 app.listen(3000);
